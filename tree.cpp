426b129aeb027d043eca270a25e6fb2e214ba1dd
kazuuuuuuuuui <kazuuuuuuuuui1105@yahoo.co.jp>
2015-11-15 17:23:25 +0900

    二分木構造の更新 書き方がわからなかったためclass→structに変更 rootの作成・要素の追加まで実装した 左又は右のノードがないところまで再帰を使って探すと言うところが難しい・・・。

--- tree.cpp
+++ tree.cpp
@@ -1,25 +1,67 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-class TreeNode{
-public:
+struct TreeNode{
 	int m_value;
 	TreeNode *m_leftNode;
 	TreeNode *m_rightNode;
-
-	//数字を受け取ってノードを生成し自身のアドレスを返す
-	TreeNode CreateNewNode(int _num);
 };
 
-TreeNode TreeNode::CreateNewNode(int _num){
-	TreeNode *node = new TreeNode();
-	node->m_value = _num;
-	node->m_leftNode = NULL;
-	node->m_rightNode = NULL;
+TreeNode *root;
 
-	return /*nodeを返す*/;
+//数字を受け取ってノードを生成し自身のアドレスを返す
+TreeNode* CreateNewNode(int _num){
+	TreeNode *newNode = (TreeNode*)malloc(sizeof(TreeNode));
+	newNode->m_value = _num;
+	newNode->m_leftNode = NULL;
+	newNode->m_rightNode = NULL;
+
+	return newNode;
+}
+
+//木にノードを追加
+//*_parentNodeは追加するノードの親を示すポインタ
+void InsertNode(TreeNode *_parentNode, int _num){
+	//rootの生成
+	if (_parentNode == NULL){
+		root = CreateNewNode(_num);
+		return;
+	}
+
+	//親＞子
+	if (_parentNode->m_value > _num){
+		//左がNULLじゃなかったらNULLになるまで回す
+		if (_parentNode->m_leftNode != NULL){
+			InsertNode(_parentNode->m_leftNode, _num);
+		}
+		//左がNULLだったらそこにノードを作ってvalue追加
+		else{
+			_parentNode->m_leftNode = CreateNewNode(_num);
+		}
+	}
+
+	//親＜子
+	else{
+		if (_parentNode->m_rightNode != NULL){
+			InsertNode(_parentNode->m_rightNode, _num);
+		}
+		else{
+			_parentNode->m_rightNode = CreateNewNode(_num);
+		}
+	}
 }
 
 void main(){
-	TreeNode *root = NULL;
+	root = NULL;
+
+	InsertNode(root, 10);
+	InsertNode(root, 5);
+	InsertNode(root, 2);
+
+	printf("%d\n", root->m_value);
+	printf("%d\n", root->m_leftNode->m_value);
+	printf("%d\n", root->m_leftNode->m_leftNode->m_value);
+	getchar();
+
 
 }
